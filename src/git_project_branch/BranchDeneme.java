6b4488e97d62a53258548f089d2a8b83afbc3b56
Mehmet <myesil3445@gmail.com>
2020-08-20 18:18:09 +0200

    ilk commit from master

--- src/git_project_branch/BranchDeneme.java
+++ src/git_project_branch/BranchDeneme.java
@@ -3,7 +3,8 @@ package git_project_branch;
 public class BranchDeneme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("16:28master branch'ten ilk commit");
 
 	}
 
