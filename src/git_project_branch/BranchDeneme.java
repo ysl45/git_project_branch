75ffa52e52d50fe84ee651d71d77ebc7ad78c8f3
Mehmet <myesil3445@gmail.com>
2020-08-20 18:29:41 +0200

    feature'den commit

--- src/git_project_branch/BranchDeneme.java
+++ src/git_project_branch/BranchDeneme.java
@@ -5,6 +5,7 @@ public class BranchDeneme {
 	public static void main(String[] args) {
 		
 		System.out.println("16:28master branch'ten ilk commit");
+		System.out.println("18:36 feature branch'teni ilk commit");
 
 	}
 
